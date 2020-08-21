97633375018db075676ec9476842a549d1e86099
Your NameAlicia <alicialwilliamsnyc@gmail.com>
2020-08-21 03:02:09 -0400

    Need-to-correct-name

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import {Route,Switch} from 'react-router-dom';
+
 import './App.css';
-import Welcome from './components/welcome/Welcome.js';
-import Clock from './components/clock/Clock.js';
-import Contact from './components/contact/Contact.js'
+import Welcome from './components/welcome/Welcome';
+import Clock from './components/clock/Clock';
+import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Alicia" />
-      <Clock />
-      <Contact />
+      <Navigation />
+        <Switch>
+          <Route exact path='/' render={()=> <div>Home</div>} />
+          <Route  
+          path="/Welcome/:name"
+          render={() => <h1>Welcome, {Welcome}</h1>}
+        />
+          <Route path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+          <Route render = {
+            ()=> <h3>Page Not Found</h3>
+          } />
+        </Switch>
     </div>
   );
 }
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,11 +1,21 @@
 import React from 'react';//package we are importing is lower case
 
-function Welcome(props) {
+/*function Welcome(props) {
     return (
       <div className="Welcome">
-       <h2> Welcome, {props.name}!</h2>
+        <h2> Welcome, {this.props.match.params.name}!</h2>
+       
       </div>
     );
   }
-
+*/
+class Welcome extends React.Component{
+  render(){
+    console.log(this.props);
+    
+  return(
+    <h2>Welcome,({this.props.match.params.name})!</h2>
+    );
+  }
+}
 export default Welcome;
